7ba8c47443856ba003af7ff9427901fbc60196a1
Tom Webber <t.c.w.webber@gmail.com>
2025-08-03 13:29:25 +0200

    feat(data): add raw data in arrays

---
+++ src/raw/date.rs
@@ -0,0 +1,30 @@
+pub const DAYS_OF_WEEK: [&str; 7] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+
+pub const MONTHS: [&str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+pub const DAYS_OF_WEEK_ABBR: [&str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+pub const MONTHS_ABBR: [&str; 12] = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+];
