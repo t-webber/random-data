46fc118cf0b67e3d5f22f0be7c6f4edeb5231f44
Tom Webber <t.c.w.webber@gmail.com>
2025-10-04 10:34:14 +0200

    feat(generator): implement From<ThreadRng> for use with external rng

--- src/generator.rs
+++ src/generator.rs
@@ -1,11 +1,30 @@
 use rand::rngs::ThreadRng;
 
 /// Generator provded to the data types. Refer to [`DataType`](crate::DataType) for usage information.
+///
+/// # Examples
+///
+/// ```
+/// use random_data::DataGenerator;
+/// use rand;
+///
+/// // Create a default generator
+/// let _ = DataGenerator::default();
+///
+/// // Create a generator with a custom random
+/// let _ = DataGenerator::from(rand::rng());
+/// ```
 #[derive(Default)]
 pub struct DataGenerator(ThreadRng);
 
 impl DataGenerator {
     pub(crate) const fn rng(&mut self) -> &mut ThreadRng {
         &mut self.0
+    }
+}
+
+impl From<ThreadRng> for DataGenerator {
+    fn from(value: ThreadRng) -> Self {
+        Self(value)
     }
 }
